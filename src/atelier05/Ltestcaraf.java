78a005d006b99d7f9f6abf529f4ee88232595dd9
isha <isha@fedora>
2023-11-09 12:05:06 +0100

    atelier 05

---
+++ src/atelier05/Ltestcaraf.java
@@ -0,0 +1,17 @@
+package atelier05;
+
+public class Ltestcaraf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Carafe c1, c2;
+
+		c1 = new Carafe(5);
+		System.out.println(c1.Capacité());
+		System.out.println(c1.Contenu());
+		
+		c1 = new Carafe(7);
+
+	}
+
+}
